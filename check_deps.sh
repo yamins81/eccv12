b71451765a8adb998779275a11d35de08921f634
James Bergstra <james.bergstra@gmail.com>
2012-01-27 13:53:44 -0500

    check_deps.sh supports push

--- check_deps.sh
+++ check_deps.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
-for DIR in thoreano Theano scikit-data eccv12 hyperopt asgd ; do
 
-    echo ''
-    echo '==========================================================='
-    echo $DIR
-    echo '==========================================================='
-    pushd ../$DIR > /dev/null
+DIRS="thoreano Theano scikit-data eccv12 hyperopt asgd"
 
-    git status -s
+if [ " st" = " $1" -o " " = " $1" ] ; then
+    for DIR in $DIRS; do
 
-    popd > /dev/null
-done
+        echo ''
+        echo '==========================================================='
+        echo $DIR
+        echo '==========================================================='
+        pushd ../$DIR > /dev/null
+
+        git status -s
+
+        popd > /dev/null
+    done
+fi
+
+if [ " push" = " $1" ] ; then
+    for DIR in $DIRS ; do
+
+        echo ''
+        echo '==========================================================='
+        echo $DIR
+        echo '==========================================================='
+        pushd ../$DIR > /dev/null
+
+        git push
+
+        popd > /dev/null
+    done
+fi
+
