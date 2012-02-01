36186bb905911fcda879c6e8a2f7c9a68e8d6f59
James Bergstra <james.bergstra@gmail.com>
2012-02-01 14:12:48 -0500

    tons of work on toyproblem

--- eccv12/toyproblem.py
+++ eccv12/toyproblem.py
@@ -1,18 +1,103 @@
+import cPickle
 import sys
 
 import numpy as np
 
+from hyperopt import Ctrl
 from hyperopt.genson_helpers import choice
 from hyperopt.genson_helpers import uniform
 
-from skdata.iris import Iris
+from skdata.digits import Digits
 
 from .bandits import BaseBandit
 from .plugins import train_linear_svm_w_decisions
 from .plugins import result_binary_classifier_stats
 from .plugins import svm_decisions
+from .plugins import attach_svmasgd
+from .plugins import register
 
-class BoostableIris(BaseBandit):
+
+@register()
+def digits_xy():
+    """
+    Turn digits to binary classification: 0-4 vs. 5-9
+    """
+    X, y = Digits().classification_task()
+    y = (y < 5) * 2 - 1 # -- convert to +-1
+    return X, y
+
+
+@register(call_w_scope=True)
+def scope_lookup(key, scope):
+    return scope[key]
+
+
+@register()
+def digits_split(split_num, n_examples):
+    rstate = np.random.RandomState(42)
+    splits = rstate.permutation(n_examples).reshape(5, -1)
+    result = dict(split=[])
+
+    test_idxs = splits[split_num]
+    train_idxs = np.asarray([splits[ii]
+        for ii in range(5) if ii != split_num]).flatten()
+
+    return train_idxs, test_idxs
+
+
+@register()
+def Xy_split(Xy, decisions, idxs):
+    X, y = Xy
+    train_idxs, test_idxs = idxs
+
+    return [
+            (X[train_idxs], y[train_idxs], decisions[train_idxs]),
+            (X[test_idxs], y[test_idxs], decisions[test_idxs])
+            ]
+
+@register()
+def normalize_Xcols(train_test):
+    train, test = train_test
+    X_train, y_train, d_train = train
+    X_test, y_test, d_test = test
+
+    Xm = X_train.mean(axis=0)
+    Xs = X_train.std(axis=0) + 1e-7
+
+    # think and add tests before working in-place here
+    X_train = (X_train - Xm) / Xs
+    X_test = (X_test - Xm) / Xs
+
+    return [
+            (X_train, y_train, d_train),
+            (X_test, y_test, d_test),
+            ]
+
+@register()
+def features(config, train_test):
+    # -- unpack args
+    train, test = train_test
+    X_train, y_train, d_train = train
+    X_test, y_test, d_test = test
+
+    # -- allocate our model
+    rstate = np.random.RandomState(config['seed'])
+    W = rstate.randn(X_train.shape[1], config['n_features']) * config['scale']
+    b = rstate.randn(config['n_features']) * config['scale']
+
+    # -- apply it
+    def sigmoid(X):
+        return 1.0 / (1.0 + np.exp(-X))
+    f_train = sigmoid(np.dot(X_train, W) + b)
+    f_test = sigmoid(np.dot(X_test, W) + b)
+
+    return [
+            (f_train, y_train, d_train),
+            (f_test, y_test, d_test),
+            ]
+
+
+class BoostableDigits(BaseBandit):
     param_gen = dict(
             seed=choice([1, 2, 3, 4, 5]),
             n_features=choice([1, 5, 10]),
@@ -20,96 +105,68 @@ class BoostableIris(BaseBandit):
             )
 
     def evaluate(self, config, ctrl):
-        X, y = Iris().classification_task()
-        y = (y == 0) * 2 - 1 # -- convert to +-1
+        X, y = Xy = digits_xy()
         print len(y)
-        decisions = getattr(self, 'decisions', None)
-        if decisions is None:
-            decisions = np.zeros_like(y)
-        rstate = np.random.RandomState(42)
-        splits = rstate.permutation(len(y)).reshape(5, -1)
-        result = dict(split=[])
 
-        split_decisions = []
+        split_decisions = getattr(self, 'split_decisions', None)
+        if split_decisions is None:
+            split_decisions = [np.zeros_like(y) for ii in range(5)]
+        scope=dict(
+                ctrl=ctrl,
+                result={'split':[]},
+                )
 
         for split_idx in range(5):
             split_result = {}
-            test_idxs = splits[split_idx]
-            train_idxs = np.asarray([splits[ii]
-                for ii in range(5) if ii != split_idx]).flatten()
+            decisions = np.asarray(split_decisions[split_idx])
+            scope['decisions'] = decisions
 
-            split_result['test_idxs'] = list(test_idxs)
+            train_test_idxs = digits_split(split_idx, (len(y) // 5) * 5)
+            train_test = Xy_split(Xy, decisions, train_test_idxs)
+            train_test_norm = normalize_Xcols(train_test)
+            train_test_feat = features(config, train_test_norm)
+            train_test_feat_norm = normalize_Xcols(train_test_feat)
 
-            X_train = X[train_idxs]
-            y_train = y[train_idxs]
-            d_train = decisions[train_idxs]
-            X_test = X[test_idxs]
-            y_test = y[test_idxs]
-            d_test = decisions[test_idxs]
-
-            # XXX: where is sklearn plugin for this?
-            Xm = X_train.mean(axis=0)
-            Xs = X_train.std(axis=0) + 1e-7
-
-            X_train = (X_train - Xm) / Xs
-            X_test = (X_test - Xm) / Xs
-
-
-            # -- allocate our model
-            rstate = np.random.RandomState(config['seed'])
-            W = rstate.randn(X.shape[1], config['n_features']) * config['scale']
-            b = rstate.randn(config['n_features']) * config['scale']
-
-            # -- apply it
-            def sigmoid(X):
-                return 1.0 / (1.0 + np.exp(-X))
-            f_train = sigmoid(np.dot(X_train, W) + b)
-            f_test = sigmoid(np.dot(X_test, W) + b)
+            (f_train, y_train, d_train) = train_test_feat_norm[0]
+            (f_test, y_test, d_test) = train_test_feat_norm[1]
 
             svm = train_linear_svm_w_decisions(
-                    (X_train, y_train),
+                    (f_train, y_train),
                     l2_regularization=1e-3,
                     decisions=d_train)
 
             new_d_train = svm_decisions(
                     svm,
-                    (X_train, y_train),
+                    (f_train, y_train),
                     d_train)
 
             new_d_test = svm_decisions(
                     svm,
-                    (X_test, y_test),
+                    (f_test, y_test),
                     d_test)
 
             result_binary_classifier_stats(
-                    (X_train, y_train),
-                    (X_test, y_test),
+                    (f_train, y_train),
+                    (f_test, y_test),
                     new_d_train,
                     new_d_test,
                     scope=dict(result=split_result)
                     )
 
-            split_decisions.append(new_d_test)
-            result['split'].append(split_result)
+            attach_svmasgd(svm, 'svm', scope=dict(ctrl=ctrl))
 
-        all_idxs = splits.flatten()
-        all_decisions = np.asarray(split_decisions).flatten()
+            split_result['test_idxs'] = list(train_test_idxs[1])
+            split_result['new_d_train'] = list(new_d_train)
+            split_result['new_d_test'] = list(new_d_test)
+            scope['result']['split'].append(split_result)
 
-        decisions = all_decisions[np.argsort(all_idxs)]
-        # XXX: how to test that this indexing is right?
-        result['decisions'] = list(decisions)
-        return result
-
-
-def test_for_smoke():
-    BI = BoostableIris()
-    result = BI.evaluate(
-            config=dict(
-                seed=1,
-                n_features=5,
-                scale=2.2),
-            ctrl=None)
-    assert 'decisions' in result
+        decisions = np.zeros((5, len(y)))
+        for ii, rr in enumerate(scope['result']['split']):
+            train_idxs, test_idxs = digits_split(ii, (len(y) // 5) * 5)
+            decisions[ii][train_idxs] = rr['new_d_train']
+            decisions[ii][test_idxs] = rr['new_d_test']
+        scope['result']['decisions'] = [list(dd) for dd in decisions]
+        return scope['result']
 
 
 def test_replicable():
@@ -117,7 +174,8 @@ def test_replicable():
             seed=1,
             n_features=5,
             scale=2.2)
-    r0, r1, r2 = [BoostableIris().evaluate(config, None) for ii in range(3)]
+    ctrls = [Ctrl() for ii in range(3)]
+    r0, r1, r2 = [BoostableDigits().evaluate(config, ctrl) for ctrl in ctrls]
 
     for f in [
             lambda r: r['split'][0]['train_prediction'],
@@ -128,41 +186,120 @@ def test_replicable():
         assert f(r0) == f(r1)
         assert f(r0) == f(r2)
 
+    svm0 = cPickle.loads(ctrls[0].attachments['svm'])
+    svm1 = cPickle.loads(ctrls[1].attachments['svm'])
+    assert np.allclose(svm0['weights'], svm1['weights'])
+
     assert r0 == r1
+
+
+def test_indexing():
+    BI = BoostableDigits()
+    result = BI.evaluate(
+            config=dict(
+                seed=1,
+                n_features=5,
+                scale=2.2),
+            ctrl=Ctrl())
+    decisions = result['decisions']
+
+    for rr in result['split']:
+        print rr.keys()
+        ti0, ti1 = rr['test_idxs'][:2]
+        td0, td1 = rr['new_d_test'][:2]
+        ti2, ti3 = rr['test_idxs'][-2:]
+        td2, td3 = rr['new_d_test'][-2:]
+
+        assert decisions[ti0] == td0
+        assert decisions[ti1] == td1
+        assert decisions[ti2] == td2
+        assert decisions[ti3] == td3
+
 
 
 def test_decisions_do_something():
     config=dict(seed=1, n_features=5, scale=2.2)
 
-    B0 = BoostableIris()
-    r0 = B0.evaluate(config, None)
+    ctrl0 = Ctrl()
+    ctrl1 = Ctrl()
 
-    B1 = BoostableIris()
+    B0 = BoostableDigits()
+    r0 = B0.evaluate(config, ctrl0)
+
+    B1 = BoostableDigits()
     B1.decisions = np.random.RandomState(32).randn(150)
-    r1 = B1.evaluate(config, None)
+    r1 = B1.evaluate(config, ctrl1)
 
-    for f in [
-            lambda r: r['split'][0]['train_prediction'],
-            lambda r: r['split'][4]['train_prediction'],
-            lambda r: r['split'][2]['test_prediction'],
-            lambda r: r['split'][3]['test_prediction'],
-            ] :
-        assert f(r0) != f(r1)
+    # contrast test_replicable where they come out the same
+    svm0 = cPickle.loads(ctrl0.attachments['svm'])
+    svm1 = cPickle.loads(ctrl1.attachments['svm'])
+    assert not np.allclose(svm0['weights'], svm1['weights'])
+
+
+def test_overfitting():
+    for log_n_features in range(7, 13):
+        BI = BoostableDigits()
+        result = BI.evaluate(
+                config=dict(
+                    seed=1,
+                    n_features=2 ** log_n_features,
+                    scale=2.2),
+                ctrl=Ctrl())
+        for key in 'train_accuracy', 'test_accuracy':
+            print 2 ** log_n_features, key,
+            print np.mean([r[key] for r in result['split']])
+
+    # XXX: assert something ... what does this prove?
 
 
 def test_boosting_for_smoke():
 
-    decisions = np.zeros(150)
-    for i in range(100):
-        BI = BoostableIris()
+    n_rounds = 16
+    n_features_per_round = 16
+
+    # -- train jointly
+    print 'Training jointly'
+    BI = BoostableDigits()
+    result = BI.evaluate(
+            config=dict(
+                seed=1,
+                n_features=n_rounds * n_features_per_round,
+                scale=2.2),
+            ctrl=Ctrl())
+    for key in 'train_accuracy', 'test_accuracy':
+        print key, np.mean([r[key] for r in result['split']])
+
+    # -- train just one
+    print 'Training one round'
+    BI = BoostableDigits()
+    result = BI.evaluate(
+            config=dict(
+                seed=1,
+                n_features=n_features_per_round,
+                scale=2.2),
+            ctrl=Ctrl())
+    for key in 'train_accuracy', 'test_accuracy':
+        print key, np.mean([r[key] for r in result['split']])
+
+    # -- train in rounds
+    print 'Training in rounds'
+    decisions = None
+    for round_ii in range(n_rounds):
+        BI = BoostableDigits()
         BI.decisions = decisions
         result = BI.evaluate(
                 config=dict(
-                    seed=1,
-                    n_features=5,
+                    seed=round_ii,
+                    n_features=n_features_per_round,
                     scale=2.2),
-                ctrl=None)
-        decisions = decisions + result['decisions']
-        print abs(decisions).mean()
-
+                ctrl=Ctrl())
+        if decisions is None:
+            decisions = np.asarray(result['decisions'])
+        else:
+            decisions += result['decisions']
+        assert len(decisions) == 5
+        assert decisions.ndim == 2
+        print 'abs decisions', abs(decisions).mean()
+        for key in 'train_accuracy', 'test_accuracy':
+            print key, np.mean([r[key] for r in result['split']])
 
