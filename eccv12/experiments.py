d89ccc0fd2fb0f304639595bd5f593832704c4c1
James Bergstra <james.bergstra@gmail.com>
2012-02-14 17:23:02 -0500

    m

--- eccv12/experiments.py
+++ eccv12/experiments.py
@@ -28,26 +28,31 @@ class SerialMixin(object):
 ##############
 
 class SimpleMixture(object):
-    def __init__(self, exp):
-        self.exp = exp
+    def __init__(self, trials, bandit):
+        self.trials = trials
+        self.bandit = bandit
 
     def mix_inds(self, A):
-        exp = self.exp
-        assert len(exp.results) >= A
-        losses = np.array([x['loss'] for x in exp.results])
-        s = losses.argsort()
+        specs = self.trials.specs
+        results = self.trials.results
+        assert len(results) >= A
+        losses = map(self.bandit.loss, results, specs)
+        if None in losses:
+            raise NotImplementedError()
+        s = np.asarray(losses).argsort()
         return s[:A], np.ones((A,)) / float(A)
 
     def mix_models(self, A):
-        exp = self.exp
+        specs = self.trials.specs
         inds, weights = self.mix_inds(A)
-        return [exp.trials[ind] for ind in inds], weights
+        return [specs[ind] for ind in inds], weights
 
 
 class AdaboostMixture(SimpleMixture):
     def fetch_labels(self, splitname):
-        exp = self.exp
-        labels = np.array([_r['labels'][splitname] for _r in exp.results])
+        specs = self.trials.specs
+        results = self.trials.results
+        labels = np.array([_r['labels'][splitname] for _r in results])
         assert (labels == labels[0]).all()
         assert labels.ndim == 2
         assert set(np.unique(labels)) == set([-1, 1])
--- eccv12/snowbird.py
+++ eccv12/snowbird.py
@@ -1,21 +1,47 @@
 """
 Testing experiment classes
 """
+import cPickle
+import sys
+import matplotlib.pyplot
 
 import hyperopt
+import hyperopt.plotting
 
 from eccv12.experiments import BoostingAlgo
+from eccv12.experiments import AdaboostMixture
 
 from eccv12.toyproblem import BoostableDigits
 
+def fat_feature_boostable_digits(multiplier):
+    from pyll import scope, clone, as_apply
+    class FatFeatures(BoostableDigits):
+        param_gen = dict(
+                n_examples_train=1250,
+                n_examples_test=500,
+                n_folds=5,
+                feat_spec=dict(
+                    seed=scope.one_of(1, 2, 3, 4, 5),
+                    n_features=scope.one_of(*[
+                        multiplier * nn for nn in [2, 5, 10]]),
+                    scale=scope.uniform(0, 5)
+                    ),
+                svm_l2_regularization=1e-3,
+                decisions=None,
+                svm_max_observations=20000,
+                )
+    return FatFeatures()
 
-def boost_digits(n_trials=10, round_len=1):
+
+def boost_digits(n_trials, round_len, multiplier=1):
     trials = hyperopt.Trials()
     calls = [0]
-    bandit = BoostableDigits()
-    algo = hyperop.Random(bandit)
-    boosting_algo = experiments.BoostingAlgo(algo,
-                round_len=round_len)
+    if multiplier > 1:
+        bandit = fat_feature_boostable_digits(multiplier)
+    else:
+        bandit = BoostableDigits()
+    algo = hyperopt.Random(bandit)
+    boosting_algo = BoostingAlgo(algo, round_len=round_len)
     exp = hyperopt.Experiment(
             trials,
             boosting_algo,
@@ -30,25 +56,49 @@ def boost_digits(n_trials=10, round_len=1):
         open('snowbird.boost_digits_%i_%i.pkl' % (n_trials, round_len),
             'w'))
 
+
 def boost_digits_show(n_trials=10, round_len=1):
     dct = cPickle.load(
             open('snowbird.boost_digits_%i_%i.pkl' % (n_trials, round_len)))
     trials = dct['trials']
     bandit = BoostableDigits()
+    algo = hyperopt.Random(bandit)
+    boosting_algo = BoostingAlgo(algo, round_len=round_len)
 
-    cutoff = 0
-    while cutoff < len(trials):
-        best_trials = boosting_algo.best_by_round(list(trials))
-        new_mixture_score = bandit.score_mixture(best_trials,
-                partial_svm=True)
-        assert new_mixture_score < last_mixture_score
-        last_mixture_score = new_mixture_score
-        import matplotlib; matplotlib.use("Qt4Agg");
-        import matplotlib.pyplot as plt;
-        import numpy as np; plt.plot(np.random.rand(50)); plt.show()
+    best_trials = boosting_algo.best_by_round(list(trials))
+    train_err_rates, test_err_rates = bandit.score_mixture_partial_svm(
+            best_trials)
+
+    # XXX put in .matplotlibrc
+    hyperopt.plotting.main_plot_history(trials, bandit, do_show=False)
+
+    best_times = range(0, len(trials), round_len)
+    matplotlib.pyplot.scatter(best_times, train_err_rates, c='g')
+    matplotlib.pyplot.scatter(best_times, test_err_rates, c='r')
 
 
+    matplotlib.pyplot.show()
 
 
-def boost_digits_pure():
+def after_the_fact_adaboost(orig_n_trials, orig_round_len, boosting_rounds):
+    dct = cPickle.load(
+            open('snowbird.boost_digits_%i_%i.pkl' % (n_trials, round_len)))
+    trials = dct['trials']
+    bandit = BoostableDigits()
+
+    ada = AdaboostMixture(trials, bandit)
+    ada_inds, ada_weights = ada.mix_inds(boosting_rounds)
+
+    print ada_inds
+
+
+def main():
+    cmd = sys.argv[1]
+    args = [eval(aa) for aa in sys.argv[1:]]
+    return args[0](*args[1:])
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main())
 
