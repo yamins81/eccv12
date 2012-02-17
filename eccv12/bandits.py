715145613610a29b264c909a52efdae64096cffb
Dan Yamins <dyamins@gmail.com>
2012-02-17 10:09:58 -0500

    bandit comments

--- eccv12/bandits.py
+++ eccv12/bandits.py
@@ -7,11 +7,16 @@ import hyperopt
 
 
 def validate_result(result):
+    """this checks that the result has the right format for being used by both
+    the boosting bandit algo as well as the adaboost mixture 
+    decs are a matrix of decisions of shape (num_splits, num_examples)
+    labels is a 1-d array of binary lables
+    """
     decs = np.asarray(result['decisions'])
     assert decs.ndim == 2
     labs = np.asarray(result['labels'])
     assert labs.ndim == 1
-    assert decs.shape[0] == len(labs)
+    assert decs.shape[1] == len(labs)
     
     
 def validate_config(config):
