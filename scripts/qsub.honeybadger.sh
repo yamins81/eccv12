8521a2fd25aa3292367ffa028bd5f816ceb16198
James Bergstra <james.bergstra@gmail.com>
2012-02-27 15:56:47 -0500

    updating qsub script

--- scripts/qsub.honeybadger.sh
+++ scripts/qsub.honeybadger.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+### ADD THIS TO fabfile.py, construct dbname automatically
+
 export SCIKIT_DATA=/scratch_local/skdata
 L=$SCIKIT_DATA/lfw/aligned
 mkdir -p $L
@@ -7,5 +9,5 @@ rsync -a ~/.skdata/lfw/aligned/ $L/
 
 . VENV/eccv12/bin/activate
 VENV/eccv12/src/eccv12/hyperopt/bin/hyperopt-mongo-worker \
-    --mongo=honeybadger:44556/try2 \
+    --mongo=honeybadger:44556/try3 \
     --workdir=/scratch_local/eccv12.workdir
