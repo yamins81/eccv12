97c69d1b3af7102b8d71f625cbd4b73cbe98aaf3
James Bergstra <james.bergstra@gmail.com>
2012-03-13 10:05:18 -0400

    cifar10

--- eccv12/cifar10.py
+++ eccv12/cifar10.py
@@ -1,111 +1,33 @@
 import time
 import numpy as np
 
-import theano
-
-from skdata import larray
-from skdata.cifar10 import CIFAR10
-
-# install some functions from pyll.scope into the module symbol table
 import pyll
-
-############
-############ CODE THAT SHOULD BE UPSTREAM: HYPEROPT?
-############
-
-#@pyll.scope.define
-def pyll_theano_batched_lmap((in_rows, out_rows), seq, batchsize,
-        _debug_call_counts=None, print_progress=False):
-
-    in_shp = (batchsize,) + seq.shape[1:]
-    batch = np.zeros(in_shp, dtype='float32')
-    s_ibatch = theano.shared(batch)
-    s_xi = (s_ibatch * 1).type() # get a TensorType w right ndim
-    s_N = s_xi.shape[0]
-    s_X = theano.tensor.set_subtensor(s_ibatch[:s_N], s_xi)
-    memo = {in_rows: (s_X, in_shp)}
-    s_obatch, oshp = pyll.rec_eval(out_rows, memo=memo)
-    assert oshp[0] == batchsize
-
-    # Compile a function that takes a variable number of elements in,
-    # returns the same number of processed elements out,
-    # but does all internal computations using a fixed number of elements,
-    # because convolutions are fastest when they're hard-coded to a certain
-    # size.
-    fn = theano.function([s_xi], s_obatch[:s_N],
-            updates={
-                s_ibatch: s_X, # this allows the inc_subtensor to be in-place
-                })
-
-    def fn_1(x):
-        if _debug_call_counts:
-            _debug_call_counts['fn_1'] += 1
-        return fn(x[None, :, :, :])[0]
-
-    attrs = {
-            'shape': oshp[1:],
-            'ndim': len(oshp) -1,
-            'dtype': s_obatch.dtype }
-    def rval_getattr(attr, objs):
-        # -- objs don't matter to the structure of the return value
-        try:
-            return attrs[attr]
-        except KeyError:
-            raise AttributeError(attr)
-
-    fn_1.rval_getattr = rval_getattr
-
-    def f_map(X):
-        if _debug_call_counts:
-            _debug_call_counts['f_map'] += 1
-        rval = np.zeros((len(X),) + oshp[1:], dtype=s_obatch.dtype)
-        offset = 0
-        while offset < len(X):
-            if print_progress:
-                print 'pyll_theano_batched_lmap.f_map', offset, len(X)
-            xi = X[offset: offset + batchsize]
-            rval[offset:offset + len(xi)] = fn(xi)
-            offset += len(xi)
-        return rval
-
-    return larray.lmap(fn_1, seq, f_map=f_map)
-
-
-@pyll.scope.define
-def linsvm_train_valid_test(features, labels, n_train, n_test,
-        C=1.0, allow_inplace=False, normalize_cols=True, shuffle=False):
-    return {'result': 'awesome'}
-
-
-############
-############
-############
-
-def restrict_scope(scope, *args, **kwargs):
-    rval = {}
-    for k in args:
-        rval[k] = getattr(scope, k)
-    for k, origk in kwargs.items():
-        rval[k] = getattr(scope, origk)
-    return rval
-
-
+from pyll.base import partial1
+from pyll.base import Lambda
+from pyll import scope
 import pyll_slm
-globals().update(restrict_scope(pyll.scope,
-            #'pyll_theano_batched_lmap',
-            'linsvm_train_valid_test',
-            'uniform',
-            'quniform',
-            'loguniform',
-            'qloguniform',
-            'one_of',
-            'choice',
-            **{
-                'lnorm': 'slm_lnorm',
-                'lpool': 'slm_lpool',
-                'fbcorr': 'slm_fbcorr',
-                's_int': 'int',
-                }))
+
+pyll.scope.import_(globals(),
+    'pyll_theano_batched_lmap',
+    'cifar10_img_classification_task',
+    'linsvm_train_test',
+    'hyperopt_set_loss',
+    'np_transpose',
+    # /begin distributions that hyperopt can tune
+    'uniform',
+    'quniform',
+    'loguniform',
+    'qloguniform',
+    'one_of',
+    'choice',
+    # /end distributions...
+    **{
+    #NEW NAME:  ORIG NAME
+    'lnorm': 'slm_lnorm',
+    'lpool': 'slm_lpool',
+    'fbcorr': 'slm_fbcorr',
+    's_int': 'int',
+    })
 
 
 def rfilter_size(smin, smax, q=1):
@@ -119,80 +41,136 @@ def logu_range(lower, upper):
     return loguniform(np.log(lower), np.log(upper))
 
 
-def experiment1(namebase):
-    img_i = pyll.Literal()
+def experiment1(namebase, n_train, n_valid, n_test,
+        use_l3='no',  # can be yes, no, maybe
+        batchsize=10,
+        C=1.0):
+    # -- set up the img_i -> top_layer SLM feature pipeline
 
-    layer0 = lnorm(img_i,
-            ker_size = rfilter_size(2, 6),
+    lnorm0 = partial1(lnorm,
+            ker_size=rfilter_size(2, 6),
             remove_mean=one_of(0, 1),
             stretch=logu_range(.1/3, 10.*3),
             threshold=logu_range(.1/3, 10.*3))
 
-    layer1 = lnorm(
-            lpool(
-                fbcorr(layer0,
-                    ker_size=rfilter_size(2, 6),
-                    n_filters=s_int(
-                        qloguniform(
-                            np.log(16 / 2.0),
-                            np.log(64),
-                            q=16)),
-                    generate=('random:uniform',
-                        {'rseed': choice(range(10, 15))})),
-                ker_size=rfilter_size(2, 5),
-                order=loguniform(np.log(1), np.log(10)),
-                stride=1),
+    fbcorr1 = partial1(fbcorr,
+            ker_size=rfilter_size(2, 6),
+            n_filters=s_int(
+                qloguniform(
+                    np.log(16 / 2.0),
+                    np.log(64),
+                    q=16)),
+            generate=('random:uniform',
+                {'rseed': choice(range(10, 15))}))
+
+    lpool1 = partial1(lpool,
+            ker_size=rfilter_size(2, 5),
+            order=loguniform(np.log(1), np.log(10)),
+            stride=1)
+
+    lnorm1 = partial1(lnorm,
             ker_size = rfilter_size(2, 5),
             remove_mean=one_of(0, 1),
             stretch=logu_range(.1/3, 10.*3),
             threshold=logu_range(.1/3, 10.*3))
 
-    layer2 = lnorm(
-            lpool(
-                fbcorr(layer1,
-                    ker_size=rfilter_size(2, 6),
-                    n_filters=s_int(
-                        qloguniform(
-                            np.log(16 / 2.0),
-                            np.log(128),
-                            q=16)),
-                    generate=('random:uniform',
-                        {'rseed': choice(range(20, 25))})),
-                ker_size=rfilter_size(2, 5),
-                order=loguniform(np.log(1), np.log(10)),
-                stride=1),
+    fbcorr2 = partial1(fbcorr,
+            ker_size=rfilter_size(2, 6),
+            n_filters=s_int(
+                qloguniform(
+                    np.log(16 / 2.0),
+                    np.log(128),
+                    q=16)),
+            generate=('random:uniform',
+                {'rseed': choice(range(20, 25))}))
+
+    lpool2 = partial1(lpool,
+            ker_size=rfilter_size(2, 5),
+            order=loguniform(np.log(1), np.log(10)),
+            stride=1)
+
+    lnorm2 = partial1(lnorm,
             ker_size = rfilter_size(2, 5),
             remove_mean=one_of(0, 1),
             stretch=logu_range(.1/3, 10.*3),
             threshold=logu_range(.1/3, 10.*3))
 
-    layer3 = lnorm(
-            lpool(
-                fbcorr(layer2,
-                    ker_size=rfilter_size(2, 6),
-                    n_filters=s_int(
-                        qloguniform(
-                            np.log(16 / 2.0),
-                            np.log(384),
-                            q=16)),
-                    generate=('random:uniform',
-                        {'rseed': choice(range(30, 35))})),
-                ker_size=rfilter_size(2, 5),
-                order=loguniform(np.log(1), np.log(10)),
-                stride=1),
+    fbcorr3 = partial1(fbcorr,
+            ker_size=rfilter_size(2, 6),
+            n_filters=s_int(
+                qloguniform(
+                    np.log(16 / 2.0),
+                    np.log(384),
+                    q=16)),
+            generate=('random:uniform',
+                {'rseed': choice(range(30, 35))}))
+
+    lpool3 = partial1(lpool,
+            ker_size=rfilter_size(2, 5),
+            order=loguniform(np.log(1), np.log(10)),
+            stride=1)
+
+    lnorm3 = partial1(lnorm,
             ker_size = rfilter_size(2, 5),
             remove_mean=one_of(0, 1),
             stretch=logu_range(.1/3, 10.*3),
             threshold=logu_range(.1/3, 10.*3))
 
-    return locals()
+    pipeline = [lnorm0,
+            fbcorr1, lpool1, lnorm1,
+            fbcorr2, lpool2, lnorm2,
+            ]
 
-    result = linsvm_train_valid_test(
-            image_features, labels,
-            50000, 10000,
-            C=1.0,
+    if 0:
+        top_layer = dict(
+                yes=layer3,
+                no=layer2,
+                maybe=one_of(layer2, layer3)
+                )[use_l3]
+
+    assert n_train + n_valid < 50000
+    assert n_test < 10000
+
+    # -- map cifar10 through the pipeline
+    all_imgs, all_labels = cifar10_img_classification_task()
+    all_imgs = np_transpose(all_imgs, (0, 3, 1, 2))
+    screen_features = pyll_theano_batched_lmap(
+            pipeline,
+            all_imgs[:n_train + n_valid],
+            batchsize=batchsize)
+    test_features = pyll_theano_batched_lmap(
+            pipeline,
+            all_imgs[50000:50000 + n_test],
+            batchsize=batchsize)
+
+    result = linsvm_train_test(
+            train=(screen_features[:n_train], all_labels[:n_train]),
+            test_sets = dict(
+                valid=(
+                    screen_features[n_train:n_train + n_valid],
+                    all_labels[n_train:n_train + n_valid]
+                    ),
+                test=(
+                    test_features,
+                    all_labels[50000:50000 + n_test],
+                    ),
+                ),
+            C=C,
+            normalize_cols=True,
             allow_inplace=True,
-            shuffle=False)
+            report={
+                'col_affine': 0,
+                'svm': 0,
+                'train.error_rate': 1,
+                'train.fit_time': 1,
+                'test.valid.error_rate': 1,
+                'test.valid.accuracy': 1,
+                'test.test.error_rate': 1,
+                'test.test.accuracy': 1,
+                'train.sgd_step_size0': 1,
+                })
+
+    result_w_loss = hyperopt_set_loss(result, 'test.valid.error_rate')
 
     return locals()
 
@@ -225,7 +203,6 @@ class TestPTBL(unittest.TestCase):
         print ff.shape
         assert self.features.shape == ff.shape
 
-
     def test1(self):
         assert self.call_counts == {'fn_1': 0, 'f_map': 0}
         self.features[0]
@@ -251,30 +228,23 @@ class TestPTBL(unittest.TestCase):
 
 
 def test_cifar10():
-    allvars = experiment1('memmap_test_cifar10')
+    allvars = experiment1('memmap_test_cifar10',
+            n_train=10, n_valid=10,  n_test=10,
+            use_l3='no',
+            batchsize=10)
+    result_w_loss = allvars['result_w_loss']
+    # make the experiment fully runnable by installing a random generator
     rng = np.random.RandomState(234)
-    img_i = allvars['img_i']
-    layer3 = allvars['layer3']
-    imgs, labels = CIFAR10().img_classification_task(dtype='float32')
-    # put imgs channel-major for Theano conv
-    print imgs.dtype
-    imgs = imgs.transpose(0, 3, 1, 2)
-    pyll.stochastic.recursive_set_rng_kwarg(layer3, rng)
+    pyll.stochastic.recursive_set_rng_kwarg(result_w_loss, rng)
     i = 0
     while True:
         try:
-            image_features = pyll_theano_batched_lmap((img_i, layer3),
-                    imgs, batchsize=1000)
+            result = pyll.rec_eval(result_w_loss)
             break
         except pyll_slm.InvalidDescription:
-            print 'Invalid', i
+            print 'Invalid Description', i
             i += 1
             continue
 
-    print 'Extracting features'
-    print image_features.shape
-    t0 = time.time()
-    image_features[:]
-    t1 = time.time()
-    print 'took', t1 - t0
+    print result
 
